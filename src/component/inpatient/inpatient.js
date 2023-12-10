ea251d9b4605f199abc1f88bcdd2167763119b95
saitarunpotnuru <saitarunpotnuru@gmail.com>
2023-12-10 11:49:26 +0530

    add

---
+++ src/component/inpatient/inpatient.js
@@ -0,0 +1,10 @@
+function InpatientComponent(){
+    return(
+        <div>
+            <h1><h3>Inpatients Kindly Visit The Hospital for more Information</h3></h1>
+        </div>
+
+    )
+}
+
+export default InpatientComponent;
